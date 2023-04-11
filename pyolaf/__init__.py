98bdec0779cde3c91e4a9dfaa97920cce1b6f4a9
Lili Karashchuk <krchtchk@gmail.com>
2023-04-11 12:49:50 -0700

    fix the imports within the package

---
+++ pyolaf/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env ipython
+
+#!/usr/bin/env python3
+
+import sys
+
+__version__ = '0.1.0'
+VERSION = __version__
+
+from . import aliasing
+from . import fftpack
+from . import geometry
+from . import lf
+from . import project
+from . import transform
